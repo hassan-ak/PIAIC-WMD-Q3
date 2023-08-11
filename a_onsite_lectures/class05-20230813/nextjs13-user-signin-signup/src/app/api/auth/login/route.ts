79c461770fda75745044db734ec2a41d667d89f0
Hassan Ali Khan <73381969+hassan-ak@users.noreply.github.com>
2023-08-12 02:47:43 +0500

    Create API Route Handler to Handle User login

---
+++ a_onsite_lectures/class05-20230813/nextjs13-user-signin-signup/src/app/api/auth/login/route.ts
@@ -0,0 +1,66 @@
+import { eq } from "drizzle-orm";
+import { compare } from "bcryptjs";
+import { ZodError } from "zod";
+import { db } from "@/lib/db/drizzle";
+import { signJWT } from "@/lib/token";
+import { jwt_users } from "@/lib/db/schema/script";
+import { NextRequest, NextResponse } from "next/server";
+import { getEnvVariable, getErrorResponse } from "@/lib/helpers";
+import { LoginUserInput, LoginUserSchema } from "@/lib/validations/user.schema";
+
+export async function POST(req: NextRequest) {
+  try {
+    const body = (await req.json()) as LoginUserInput;
+    const data = LoginUserSchema.parse(body);
+    console.log("data ===> ", data);
+    const user = await db
+      .select({
+        user_id: jwt_users.user_id,
+        password: jwt_users.password,
+      })
+      .from(jwt_users)
+      .where(eq(jwt_users.email, data.email));
+    console.log("user ===> ", user);
+    if (!user[0] || !(await compare(data.password, user[0].password))) {
+      return getErrorResponse(401, "Invalid email or password");
+    }
+    const JWT_EXPIRES_IN = getEnvVariable("JWT_EXPIRES_IN");
+    const token = await signJWT(
+      { sub: `${user[0].user_id}` },
+      { exp: `${JWT_EXPIRES_IN}m` }
+    );
+    const response = new NextResponse(
+      JSON.stringify({
+        status: "success",
+        token: token,
+      }),
+      {
+        status: 200,
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+    const tokenMaxAge = parseInt(JWT_EXPIRES_IN) * 60;
+    const cookieOptions = {
+      name: "token",
+      value: token,
+      httpOnly: true,
+      path: "/",
+      secure: process.env.NODE_ENV !== "development",
+      maxAge: tokenMaxAge,
+    };
+    await Promise.all([
+      response.cookies.set(cookieOptions),
+      response.cookies.set({
+        name: "logged-in",
+        value: "true",
+        maxAge: tokenMaxAge,
+      }),
+    ]);
+    return response;
+  } catch (error: any) {
+    if (error instanceof ZodError) {
+      return getErrorResponse(400, "failed validations", error);
+    }
+    return getErrorResponse(500, error.message);
+  }
+}
--- a_onsite_lectures/class05-20230813/nextjs13-user-signin-signup/src/lib/validations/user.schema.ts
+++ a_onsite_lectures/class05-20230813/nextjs13-user-signin-signup/src/lib/validations/user.schema.ts
@@ -1,4 +1,5 @@
 import { z } from "zod";
+
 export const RegisterUserSchema = z
   .object({
     name: z
@@ -30,4 +31,21 @@ export const RegisterUserSchema = z
     path: ["passwordConfirm"],
     message: "Passwords do not match",
   });
-export type RegisterUserInput = z.infer<typeof RegisterUserSchema>
+export type RegisterUserInput = z.infer<typeof RegisterUserSchema>;
+
+export const LoginUserSchema = z.object({
+  email: z
+    .string({
+      required_error: "Email is required",
+    })
+    .min(1, "Email is required")
+    .email("Email is invalid"),
+  password: z
+    .string({
+      required_error: "Password is required",
+    })
+    .min(1, "Password is required")
+    .min(8, "Password must be at least 8 characters")
+    .max(32, "Password must be less than 32 characters"),
+});
+export type LoginUserInput = z.infer<typeof LoginUserSchema>;
